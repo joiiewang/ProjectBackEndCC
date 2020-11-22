c2fa411adc30849ed248d57e2e1cde2eea90b79b
Joy <joywang@ucsb.edu>
2020-11-22 06:24:12 +0000

    another small tweak sorry

--- database.py
+++ database.py
@@ -7,7 +7,7 @@ class User(db.Model):
     password_hash = db.Column(db.String(128), nullable=False)
     courses = db.relationship('Course',backref='user',lazy='dynamic')
     links = db.relationship('Link',backref='user',lazy='dynamic')
-    toDoObjects = db.relationship('ToDoList',backref='user',lazy='dynamic')
+    todos = db.relationship('ToDoList',backref='user',lazy='dynamic')
     notes = db.relationship('Note',backref='user',lazy='dynamic')
 
     points = db.Column(db.Integer, index=True, nullable=False)
@@ -21,7 +21,7 @@ class Course(db.Model):
     name = db.Column(db.String(64), index=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     links = db.relationship('Link',backref='course',lazy='dynamic')
-    toDoObjects = db.relationship('ToDoList',backref='course',lazy='dynamic')
+    todos = db.relationship('ToDoList',backref='course',lazy='dynamic')
     notes = db.relationship('Note',backref='course',lazy='dynamic')
 
     def __repr__(self):
@@ -43,14 +43,14 @@ class Link(db.Model):
 
 class ToDoList(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    toDoItem = db.Column(db.String(100), index=True)
+    text = db.Column(db.String(100), index=True)
     dueDate = db.Column(db.String(8), index=True)
     user_id=db.Column(db.Integer, db.ForeignKey('user.id'))
     course_id = db.Column(db.Integer, db.ForeignKey('course.id'))
     def __repr__(self):
         return f'<ToDoList {self.toDoItem}>'
     def to_dict(self):
-        return {"id":self.id,"toDoitem":self.toDoItem,"dueDate":self.dueDate}
+        return {"id":self.id,"text":self.text,"dueDate":self.dueDate}
 
 class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
